c942a0a6096b5fb46f3753e0dedf9f399e214e28
Quint Legius <qlegius@signet.nl>
2020-11-21 11:28:46 +0100

    Fixed css

--- public/js/app.js
+++ public/js/app.js
@@ -49910,9 +49910,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/quintlegius/Documents/Projects/salvemundi.nl-laravel/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /Users/quintlegius/Documents/Projects/salvemundi.nl-laravel/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /Users/quintlegius/Documents/Projects/salvemundi.nl-laravel/resources/sass/custom.scss */"./resources/sass/custom.scss");
+__webpack_require__(/*! /home/qlegius/Documents/Projects/salvemundi.nl-laravel/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /home/qlegius/Documents/Projects/salvemundi.nl-laravel/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /home/qlegius/Documents/Projects/salvemundi.nl-laravel/resources/sass/custom.scss */"./resources/sass/custom.scss");
 
 
 /***/ })
