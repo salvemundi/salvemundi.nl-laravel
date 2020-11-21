b55564b2f222db62b4ffc97c5c613dca17a0863c
Quint Legius <qlegius@signet.nl>
2020-11-21 20:46:33 +0100

    Fixed css, profile pictures not quite working.

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,24 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="overlap">
+<div class="overlap grid">
+@foreach ($user as $users)
+
     <div class="card">
         <div class="row">
-            @foreach ($user as $users)
             <div class="col-md-4">
-                <img src="/images/headerLogoSamu.jpg" class="w-100">
+                <?php
+            echo '<img class="pfPhoto" src="data:'.';base64,'.base64_encode($imgarray[0]).'" />';
+            ?>
             </div>
             <div class="col-md-4">
-                <div class="card-block">
                     <h4 class="card-title">{{$users->getDisplayName() }}</h4>
                     <p class="card-text">{{$users->getDisplayName() }}</p>
                     <p class="card-text">{{$users->getDisplayName() }}</p>
-                </div>
             </div>
-
             {{-- <img src="data:images/jpeg;base64,{{\O365\Profile::photo()}}"/> --}}
-            @endforeach
         </div>
     </div>
+
+    @endforeach
 </div>
 @endsection
