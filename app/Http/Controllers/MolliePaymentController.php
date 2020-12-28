a451ab9739b473caf9cbe113d5727f41becb46b4
Quint Legius <quint.legius@iqfx.nl>
2020-12-28 15:26:59 +0100

    Intro fix 1.0

--- app/Http/Controllers/MolliePaymentController.php
+++ app/Http/Controllers/MolliePaymentController.php
@@ -14,6 +14,7 @@ class MolliePaymentController extends Controller
     public static function processRegistration($orderObject, $productIndex): RedirectResponse
     {
         $createPayment = MolliePaymentController::preparePayment($productIndex);
+        dd($createPayment->id);
         $transaction = new Transaction();
         $transaction->transactionId = $createPayment->id;
         $transaction->product()->associate(Product::where('index', $productIndex));
