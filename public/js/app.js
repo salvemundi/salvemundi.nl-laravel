8ea3176b73c874aab819fb904e4796b0ffde9582
Quint Legius <quint.legius@iqfx.nl>
2021-01-09 15:33:09 +0100

    app.js

--- public/js/app.js
+++ public/js/app.js
@@ -53016,12 +53016,12 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\salvemundi.nl-laravel\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\xampp\htdocs\salvemundi.nl-laravel\resources\sass\app.scss */"./resources/sass/app.scss");
-__webpack_require__(/*! C:\xampp\htdocs\salvemundi.nl-laravel\resources\sass\custom.scss */"./resources/sass/custom.scss");
-__webpack_require__(/*! C:\xampp\htdocs\salvemundi.nl-laravel\resources\sass\tabs.scss */"./resources/sass/tabs.scss");
-__webpack_require__(/*! C:\xampp\htdocs\salvemundi.nl-laravel\resources\sass\checkbox.scss */"./resources/sass/checkbox.scss");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\salvemundi.nl-laravel\resources\sass\adminCards.scss */"./resources/sass/adminCards.scss");
+__webpack_require__(/*! C:\Users\Quint\Documents\Projects\salvemundi.nl-laravel\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\Users\Quint\Documents\Projects\salvemundi.nl-laravel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Quint\Documents\Projects\salvemundi.nl-laravel\resources\sass\custom.scss */"./resources/sass/custom.scss");
+__webpack_require__(/*! C:\Users\Quint\Documents\Projects\salvemundi.nl-laravel\resources\sass\tabs.scss */"./resources/sass/tabs.scss");
+__webpack_require__(/*! C:\Users\Quint\Documents\Projects\salvemundi.nl-laravel\resources\sass\checkbox.scss */"./resources/sass/checkbox.scss");
+module.exports = __webpack_require__(/*! C:\Users\Quint\Documents\Projects\salvemundi.nl-laravel\resources\sass\adminCards.scss */"./resources/sass/adminCards.scss");
 
 
 /***/ })
