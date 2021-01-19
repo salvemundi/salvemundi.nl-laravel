18086606ec1709f3d31e61be3418e220bc69e756
Sanne <sanne2610h@gmail.com>
2021-01-19 16:35:49 +0100

    Backend activiteiten toevoegen

---
+++ resources/views/admin/activities.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.appmin')
+@section('content')
+
+<div class="row center overlap">
+    <div id="contact" class="col-md-6">
+        @if(session()->has('message'))
+        <div class="alert alert-primary">
+            {{ session()->get('message') }}
+        </div>
+        @endif
+        <form action="/admin/activities/store" method="post">
+            @csrf
+            <br>
+            <h2 class="h2">Activiteiten aanmaken</h2>
+
+            <div class="form-group">
+                <label for="voornaam">Activiteit naam</label>
+                <input class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" value="{{ old('name') }}" id="name" name="name" placeholder="Naam...">
+            </div>
+
+            <div class="form-group">
+                <label for="Achternaam">Prijs</label>
+                <input type="number" min="0" step=".01" class="form-control{{ $errors->has('price') ? ' is-invalid' : '' }}" value="{{ old('price') }}" id="price" name="price" placeholder="Prijs...">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Beschrijving</label>
+                <textarea class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" value="{{{ old('description') }}}" name="description" placeholder="Beschrijving..."></textarea>
+            </div>
+
+            <div class="form-group">
+                <br>
+                <input class="btn btn-primary" type="submit" value="Versturen">
+            </div>
+        </form>
+    </div>
+</div>
+
+@endsection
