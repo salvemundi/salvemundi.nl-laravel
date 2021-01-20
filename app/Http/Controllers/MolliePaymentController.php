3f25df6683a217431ff27a0672751f514be03ae9
Quint Legius <quint.legius@iqfx.nl>
2021-01-20 11:04:33 +0100

    Testing transaction id assignment

--- app/Http/Controllers/MolliePaymentController.php
+++ app/Http/Controllers/MolliePaymentController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
+use Laravel\Cashier\Order\Order;
 use Laravel\Cashier\SubscriptionBuilder\RedirectToCheckoutResponse;
 use Mollie\Laravel\Facades\Mollie;
 use App\Enums\paymentType;
@@ -32,8 +33,7 @@ class MolliePaymentController extends Controller
             $createPayment = MolliePaymentController::preparePayment($productIndex, $newUser);
             $getProductObject = Product::where('index', paymentType::registration)->first();
             $transaction = new Transaction();
-            $getLatestOrder = DB::table('orders')->latest()->first();
-            $transaction->transactionId = $getLatestOrder->mollie_payment_id;
+            $transaction->transactionId = Order::all()->last()->transactionId;
             $transaction->product()->associate($getProductObject);
             $transaction->save();
             $orderObject->payment()->associate($transaction);
