626feaf51274b0639364dea2c0cd91e4d89f6133
Sanne <sanne2610h@gmail.com>
2021-01-22 14:18:15 +0100

    news admin pagina met aanmaken en verwijderen en nieuws op index pagina.

--- resources/views/admin/news.blade.php
+++ resources/views/admin/news.blade.php
@@ -4,12 +4,55 @@
     function CopyMe(oFileInput, sTargetID) {
         document.getElementById(sTargetID).value = oFileInput.value;
     }
+
 </script>
+{{-- <script src="extensions/resizable/bootstrap-table-resizable.js"></script> --}}
+<div class="row center adminOverlap">
+    <div class="row">
+        <div class="col-md-12 center">
+
+            <div class="table-responsive center" >
+
+                <table
+                       id="table"
+                       data-toggle="table"
+                       data-search="true"
+                       data-sortable="true"
+                       data-pagination="true"
+                       data-show-columns="true">
+                    <thead>
+                        <tr class="tr-class-1">
+                            <th data-field="title" data-sortable="true" data-width="250">Titel</th>
+                            <th data-field="content" data-sortable="true">Inhoud</th>
+                            <th data-field="imgPath" data-sortable="true" data-width="250">Foto pad</th>
+                            <th data-field="delete" data-sortable="false">Verwijderen</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($news as $nieuws)
+                                <tr id="tr-id-3" class="tr-class-2" data-title="bootstrap table">
+                                    <td data-value="{{ $nieuws->title }}">{{$nieuws->title}}</td>
+                                    <td data-value="{{ $nieuws->content }}">{{$nieuws->content}}</td>
+                                    <td data-value="{{ $nieuws->imgPath }}">{{$nieuws->imgPath}}</td>
+                                    <td data-value="{{ $nieuws->id }}"><form method="post" action="/admin/news/delete">@csrf<input type="hidden" name="id" value="{{ $nieuws->id }}"><button type="submit" class="btn btn-danger">Verwijderen</button></form></td>
+                                </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="row center adminOverlap">
     <div id="contact" class="col-md-6">
         @if(session()->has('message'))
         <div class="alert alert-primary">
             {{ session()->get('message') }}
+        </div>
+        @endif
+        @if(session()->has('information'))
+        <div class="alert alert-success">
+            {{ session()->get('information') }}
         </div>
         @endif
         <form action="/admin/news/store" method="post" enctype="multipart/form-data">
