173802accaa8bbe210c31f54f1e4b1eb67468fa3
Sanne <sanne2610h@gmail.com>
2021-01-24 15:24:02 +0100

    Admin whatsapp link op eigen pagina en dashboard verbererd

--- resources/views/include/adminNavbar.blade.php
+++ resources/views/include/adminNavbar.blade.php
@@ -40,6 +40,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/admin/sponsors">Sponsoren</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/admin/whatsapp">Whatsapp</a>
+                </li>
             </ul>
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
