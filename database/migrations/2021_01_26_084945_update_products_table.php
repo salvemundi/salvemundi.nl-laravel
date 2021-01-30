7c1703e59aec30ff2d6a8933543a5365642e3bde
Quint Legius <quint.legius@iqfx.nl>
2021-01-30 16:13:47 +0100

    Made admin navbar wider to fit  more buttons

--- database/migrations/2021_01_26_084945_update_products_table.php
+++ database/migrations/2021_01_26_084945_update_products_table.php
@@ -13,7 +13,7 @@ class UpdateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function ($table) {
+        Schema::table('products', function ($table) {
             $table->string('formsLink')->nullable();
         });
     }
