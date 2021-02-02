ca9de9cef286a3402530869b714bdb6d05e4f08f
Sanne <sanne2610h@gmail.com>
2021-02-02 17:14:24 +0100

    dingetjes van albion denk ik

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,69 +4,71 @@
 
 
 <div class="overlap">
-    @if(session('userName'))
+    <div class="mijnSlider">
+        @if(session('userName'))
 
-    <h4>Welkom <b>{{ session('userName') }}!</h4></b>
+            <h4>Welkom <b>{{ session('userName') }}!</h4></b>
 
-@endif
-<div class="mijnSlider">
-    <h2 class="h2">Over ons</h2> <br>
-    <p>Salve Mundi is de Studievereniging van Fontys Hogenscholen ICT, opgericht in 2017 door Mohammed Mighiss en Luuk Hendriks. Het is Latijn voor "Hello World", een typische zin die menig programmeur maar al te goed kent.</p>
-    <p>De vereniging organiseert veel activiteiten, zowel studie bemiddelend als voor de lol, of een combinatie van de twee. Denk hierbij aan: <br></p>
+        @endif
 
-    <div class="row">
-        <div class="col-md-6">
-            <li>Lezingen</li>
-            <li> Excursies </li>
-            <li> Workshops </li>
-            <li> Bedrijfsbezoeken </li>
-            <li> Elke dinsdag bijlessen </li>
-            <br>
-            <p> Voor de uitgaande studenten:</p>
+        <h2 class="h2">Over ons</h2> <br>
+        <p>Salve Mundi is de Studievereniging van Fontys Hogenscholen ICT, opgericht in 2017 door Mohammed Mighiss en Luuk Hendriks. Het is Latijn voor "Hello World", een typische zin die menig programmeur maar al te goed kent.</p>
+        <p>De vereniging organiseert veel activiteiten, zowel studie bemiddelend als voor de lol, of een combinatie van de twee. Denk hierbij aan: <br></p>
 
-            <li> Op stap bij onze stamkroeg Villa Fiësta op Stratumseind </li>
-            <li> Casinoavonden </li>
-            <li> Pubquiz-avonden </li>
-            <li> Willekeurige spontane activiteiten </li>
-            <br>
-            <p> En voor de wat minder uitgaande studenten:</p>
+        <div class="row">
+            <div class="col-md-6">
+                <li>Lezingen</li>
+                <li> Excursies </li>
+                <li> Workshops </li>
+                <li> Bedrijfsbezoeken </li>
+                <li> Elke dinsdag bijlessen </li>
+                <br>
+                <p> Voor de uitgaande studenten:</p>
 
-            <li> Jaarlijks op kamp </li>
-            <li> Game Nights (bordspellen & eSports) </li>
-            <li> Partnership met een grote hackathon </li>
-            <li> Uitjes naar bijvoorbeeld pretparken </li>
-            <br>
-            <p>Naast deze activiteiten verzorgd Salve Mundi ook de introductie van FHICT van top tot teen.
-        </div>
-    
-        <div class="col-md-6">
-            <div class="imgSlider"  data-slick='{"slidesToShow": 1, "slidesToScroll": 1}'>
-                <div>
-                    <img class="imgIndex" src="images/SaMuFotos/DSC07676.jpg">
-                </div>
-                <div>
-                    <img class="imgIndex" src="images/SaMuFotos/DSC07654.jpg">
-                </div>
-                <div>
-                    <img class="imgIndex" src="images/SaMuFotos/DSC07719.jpg">
-                </div>
-                <div>
-                    <img class="imgIndex" src="images/SaMuFotos/IMG_0032.jpg">
-                </div>
-                <div>
-                    <img class="imgIndex" src="images/SaMuFotos/IMG_0215.jpg">
-                </div>
-                <div>
-                    <img class="imgIndex" src="images/SaMuFotos/IMG_0582.jpg">
-                </div>
-                <div>
-                    <img class="imgIndex" src="images/SaMuFotos/IMG_0413.jpg">
-                </div>
+                <li> Op stap bij onze stamkroeg Villa Fiësta op Stratumseind </li>
+                <li> Casinoavonden </li>
+                <li> Pubquiz-avonden </li>
+                <li> Willekeurige spontane activiteiten </li>
+                <br>
+                <p> En voor de wat minder uitgaande studenten:</p>
 
+                <li> Jaarlijks op kamp </li>
+                <li> Game Nights (bordspellen & eSports) </li>
+                <li> Partnership met een grote hackathon </li>
+                <li> Uitjes naar bijvoorbeeld pretparken </li>
+                <br>
+                <p>Naast deze activiteiten verzorgd Salve Mundi ook de introductie van FHICT van top tot teen.
+            </div>
+
+            <div class="col-md-6">
+                <div class="imgSlider"  data-slick='{"slidesToShow": 1, "slidesToScroll": 1}'>
+                    <div>
+                        <img class="imgIndex" src="images/SaMuFotos/DSC07676.jpg">
+                    </div>
+                    <div>
+                        <img class="imgIndex" src="images/SaMuFotos/DSC07654.jpg">
+                    </div>
+                    <div>
+                        <img class="imgIndex" src="images/SaMuFotos/DSC07719.jpg">
+                    </div>
+                    <div>
+                        <img class="imgIndex" src="images/SaMuFotos/IMG_0032.jpg">
+                    </div>
+                    <div>
+                        <img class="imgIndex" src="images/SaMuFotos/IMG_0215.jpg">
+                    </div>
+                    <div>
+                        <img class="imgIndex" src="images/SaMuFotos/IMG_0582.jpg">
+                    </div>
+                    <div>
+                        <img class="imgIndex" src="images/SaMuFotos/IMG_0413.jpg">
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
-</div>
+
     <div class="mijnSlider">
         <a href="/activiteiten">
             <h1 class="center groot"><b>Activiteiten</b></h1>
