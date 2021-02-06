f8532417dcfa79e788166c8f7f735c23c07a521a
Sanne <sanne2610h@gmail.com>
2021-02-06 15:48:29 +0100

    Re-did my account navbar
    xoxo quint

--- resources/views/finance.blade.php
+++ resources/views/finance.blade.php
@@ -4,12 +4,12 @@
 <div class="overlap">
     <div class ="center">
         @if($financeDocument != null)
-            <object
+            {{-- <object
             data="{{ 'storage/'.$financeDocument->filePath }}"
-            type="application/pdf"
             style="width:80%; height:1000px;"
             frameborder="0">
-        </object>
+        </object> --}}
+        <iframe src="{{ 'storage/'.$financeDocument->filePath }}" style="width:80%; height:1000px" frameborder="0"></iframe>
         @else
             <h2> er zijn geen bestanden gevonden</h2>
         @endif
