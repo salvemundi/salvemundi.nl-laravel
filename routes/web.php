d4cd2db27c5b2cf7e066a6b5f0f36a653938739b
Sanne <sanne2610h@gmail.com>
2021-05-19 23:58:35 +0200

    agenda

--- routes/web.php
+++ routes/web.php
@@ -77,6 +77,9 @@ Route::get('/responsible-disclosure', function () {
     return view("privacyZooi");
 });
 
+// agenda
+Route::get('/agenda', function() {return view('agenda');})->name('agenda');
+
 // Admin Panel
 
 Route::get('/admin', [App\Http\Controllers\AdminController::class, 'dashboard'])->middleware('admin.auth');
