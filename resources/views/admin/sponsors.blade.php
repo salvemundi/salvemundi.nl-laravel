167e896f171d8cb70730446a293dad8eb68dae75
Quint <quint.legius@iqfx.nl>
2021-06-10 19:44:40 +0200

    Fixed input fields

--- resources/views/admin/sponsors.blade.php
+++ resources/views/admin/sponsors.blade.php
@@ -65,13 +65,10 @@
             <label for="photo">Foto*</label>
             <div class="input-group mb-3 test">
                 <div class="input-group-prepend">
-                    <div class="custom-file" style="width: 80px;">
-                        <label class="input-group-text form-control" id="inputGroupFileAddon01" for="photo">Browse </label>
-                        <input type="file" onchange="CopyMe(this, 'txtFileName');" class="custom-file-input" style="height: 0px;" id="photo" name="photo" aria-describedby="inputGroupFileAddon01">
+                    <div class="mb-3">
+                        <label for="formFile" class="form-label">Upload sponsor foto</label>
+                        <input class="form-control" type="file" id="photo" name="photo">
                     </div>
-                </div>
-                <div class="custom-file form-control">
-                    <input style="border: hidden;" id="txtFileName" type="text" readonly="readonly" />
                 </div>
             </div>
 
