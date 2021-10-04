d102fcfa38ec6af667a177a3fb2fab8a50e1537f
Albion <461340@student.fontys.nl>
2021-10-04 09:26:57 +0200

    not done with clubs push

--- resources/views/clubs.blade.php
+++ resources/views/clubs.blade.php
@@ -2,11 +2,63 @@
 @section('content')
 
 <div class="overlap">
+    <div class="row mb-4">
+        @foreach ($clubs as $club)
+            <div class="px-2 mb-3 col-12 col-md-6">
+                <div class="card" style="min-height: 16em;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            {{-- <img src="..." class="img-fluid rounded-start" alt="..."> --}}
+                            {!! '<img class="img-fluid rounded-start" src="storage/'.$club->imgPath.'" />' !!}
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h2 class="card-title">{{ $club->clubName}}</h2>
+                                <p class="card-text">
+                                    @if (session('id') != null)
+                                    <div class="row">
+                                        <div class="col-auto">
+                                            <a href="{{ $club->whatsappLink }}"><button class="btn btn-primary mb-3"><i class="fab fa-whatsapp"></i> Join club</button></a> <br>
+                                        </div>
+                                        @if ($club->discordLink)
+                                            <div class="col-auto">
+                                                <a href="{{ $club->discordLink }}"><button class="btn btn-primary mb-3" href="{{ $club->discordLink }}"><i class="fab fa-discord"></i> Join Discord</button></a> <br>
+                                            </div>
+                                        @endif
+                                        @if ($club->otherLink)
+                                            <div class="col-auto">
+                                                <a href="{{ $club->otherLink}}"><button class="btn btn-primary mb-3" i class="fab fa-discord"></i> Join </button></a>  <br>
+                                            </div>
+                                        @endif
+                                    </div>
+                                    @endif
+                                    <small class="text-muted">
+                                        @if ($club->founderName != null)
+                                            <b>Naam oprichter:</b> {{ $club->founderName }} <br>
+                                        @endif
+                                    </small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="overlap">
     <div class="row justify-content-center mb-4">
         @foreach ($clubs as $club)
-            <div class="col-md-6 mb-3">
-                <div class="card">
-                    <div class="col-md-12">
+            <div class="col-md-3 col-sm-8 mb-3">
+                <div class="card ">
+                    <div class="col-auto col-md-12">
                         @if ($club->imgPath != null)
                             {!! '<img class="img-fluid foto" src="storage/'.$club->imgPath.'" />' !!}
                         @endif
@@ -33,14 +85,12 @@
                                         </div>
                                     @endif
                                 </div>
-                                <br>
                             @endif
                             @if ($club->founderName)
-                                <b>Naam oprichter:</b> {{ $club->founderName }} <br>
+                                <b>Naam oprichter:</b> {{ $club->founderName }}
                             @endif
                         </p>
                     </div>
-                    <br>
                 </div>
             </div>
         @endforeach
