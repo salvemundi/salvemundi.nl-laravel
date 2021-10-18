f497d29ac93696459c9c67df8102092cbb0e2eee
Max Korlaar <max@maxkorlaar.com>
2021-10-18 23:57:03 +0200

    Add error page views and the ability change the page title

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title', 'Toegang geweigerd')
+@section('content')
+    <div class="overlap">
+        <div class="row text-center">
+            <h1>403</h1>
+            <p>Toegang geweigerd. Je hebt momenteel geen toegang tot deze pagina.</p>
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name') }}</title>
+    <title>@yield('title', config('app.name'))</title>
 
     <!-- Scripts -->
 
