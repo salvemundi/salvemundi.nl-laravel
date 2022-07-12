329e7641e4776b1434584db8979eabb8dd49ab3b
Albion <461340@student.fontys.nl>
2022-07-12 18:33:12 +0200

    i love php

--- resources/views/admin/activitiesSignUps.blade.php
+++ resources/views/admin/activitiesSignUps.blade.php
@@ -38,9 +38,9 @@
                     @foreach($transInfo as $user)
                         <tr id="tr-id-3" class="tr-class-2" data-title="bootstrap table">
                             <td data-value=""></td>
-                            <td data-value="{{ $user->name }}">{{ $user->name }}</td>
+                            <td data-value="{{ $user[0] }}">{{ $user[0] }}</td>
                             <td data-value=""></td>
-                            <td data-value="{{ $user->email }}">{{ $user->email }}</td>
+                            <td data-value="{{ $user[1] }}">{{ $user[1] }}</td>
                             <td data-value=""></td>
                         </tr>
                     @endforeach
