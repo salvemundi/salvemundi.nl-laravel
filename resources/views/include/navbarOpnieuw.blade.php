2c26baa3f9f46568e7ff1c47690d69cebab684d0
Albion <461340@student.fontys.nl>
2022-10-18 19:41:31 +0200

    fixed some leftovers

--- resources/views/include/navbarOpnieuw.blade.php
+++ resources/views/include/navbarOpnieuw.blade.php
@@ -46,6 +46,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/clubs">Clubs</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/stickers">Stickers</a>
+                </li>
             </ul>
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav w-50 d-flex justify-content-end">
