0e17b0c0c5e97359af57c993cadeee63fe9eb0a0
Quint Legius <quint.legius@gmail.com>
2023-01-31 19:11:49 +0100

    fixed gitignore

--- database/migrations/2020_12_20_154913_update_introduction_table.php
+++ database/migrations/2020_12_20_154913_update_introduction_table.php
@@ -14,15 +14,15 @@ class UpdateIntroductionTable extends Migration
     public function up()
     {
         Schema::table('introduction', function (Blueprint $table) {
-            $table->unsignedBigInteger('paymentId')->nullable()->index();
-            $table->foreign('paymentId')->references('id')->on('transaction');
-            $table->string('firstNameParent')->nullable();
-            $table->string('lastNameParent')->nullable();
-            $table->string('addressParent')->nullable();
-            $table->string('medicalIssues')->nullable();
-            $table->string('specials')->nullable();
-            $table->string('phoneNumberParent')->nullable();
-        });
+        $table->unsignedBigInteger('paymentId')->nullable()->index();
+        $table->foreign('paymentId')->references('id')->on('transaction');
+        $table->string('firstNameParent')->nullable();
+        $table->string('lastNameParent')->nullable();
+        $table->string('addressParent')->nullable();
+        $table->string('medicalIssues')->nullable();
+        $table->string('specials')->nullable();
+        $table->string('phoneNumberParent')->nullable();
+    });
     }
 
     /**
