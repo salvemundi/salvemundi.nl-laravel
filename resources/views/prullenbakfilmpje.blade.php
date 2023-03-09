62331afa382b18ae060319a29fcbc09459cd96b5
Quint Legius <quint.legius@gmail.com>
2023-03-09 21:03:09 +0100

    added movie

--- resources/views/prullenbakfilmpje.blade.php
+++ resources/views/prullenbakfilmpje.blade.php
@@ -4,7 +4,7 @@
     <div class="overlap">
         <div class="row center">
             <h1 class="center">Waar moet de prullebak zak gelaten worden??</h1>
-            <video controls muted>
+            <video controls>
                 <source src="{{asset('/images/prullenbak.mp4')}}" type="video/mp4">
             </video>
         </div>
