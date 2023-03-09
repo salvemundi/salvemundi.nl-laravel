6ef6d362747aa65948e274a3b9a882228b76c33f
Quint Legius <quint.legius@gmail.com>
2023-03-09 21:09:38 +0100

    fixed spelling mistake

--- resources/views/prullenbakfilmpje.blade.php
+++ resources/views/prullenbakfilmpje.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="overlap">
         <div class="row center">
-            <h1 class="center">Waar moet de prullebak zak gelaten worden??</h1>
+            <h1 class="center">Waar moet de prullenbak zak gelaten worden??</h1>
             <video controls>
                 <source src="{{asset('/images/prullenbak.mp4')}}" type="video/mp4">
             </video>
