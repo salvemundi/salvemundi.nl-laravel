46bb29f4185fa1e31efe462ea9dea40eaa03b5dc
Quint Legius <quint.legius@gmail.com>
2023-07-20 00:07:20 +0200

    changed mail

--- resources/views/mail/MembershipExpiry.blade.php
+++ resources/views/mail/MembershipExpiry.blade.php
@@ -7,7 +7,7 @@ Beste Lid,<br>
     Dit kun je doen door op https://salvemundi.nl in te loggen met je Salve Mundi office account en vervolgens naar "mijn account" te navigeren. Hierna kun je op het grijze knopje "non active" klikken om je lidmaatschap te vernieuwen. Deze is dan weer een jaar geldig!
 </p>
 <p>
-    Lukt het niet om je lidmaatschap te verlengen? Aarzel vooral niet om de ICT-commissie te benaderen voor assistentie of vragen via ict@salvemundi.nl. Mocht het inloggen niet lukkken op de website, probeer eerst met incognito in te loggen op salvemundi.nl!
+    Lukt het niet om je lidmaatschap te verlengen? Aarzel vooral niet om de ICT-commissie te benaderen voor assistentie of vragen via ict@salvemundi.nl. Mocht het inloggen niet lukkken op de website, probeer eerst met incognito in te loggen op https://salvemundi.nl!
 </p>
 <p>
     Voor overige vragen met betrekking tot jouw lidmaatschap kun je altijd het bestuur benaderen. (bestuur@salvemundi.nl)
