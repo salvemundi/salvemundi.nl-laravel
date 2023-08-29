4bb5fc94e93599ed1558402a96a4bc21413d6d81
Quint <quint.legius@gmail.com>
2023-08-29 15:47:59 +0200

    added html attribute

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -70,7 +70,7 @@
 </nav>
 {{-- <img class="navImg" src="/images/headerLogoSamu.jpg"> --}}
 <div class="overlayVideo">
-    <video class="navImg" autoplay muted loop disablePictureInPicture id="vid">
+    <video class="navImg" autoplay muted playsinline loop disablePictureInPicture id="vid">
         <source src="{{asset('/images/intro2023.mp4')}}" type="video/mp4">
         Your browser does not support the video tag.
     </video>
