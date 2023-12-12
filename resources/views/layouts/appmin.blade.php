1241976aeb45a9968c83b93eb4d9029696602a4b
Quint <quint.legius@gmail.com>
2023-12-12 15:19:38 +0100

    Made app PWA

--- resources/views/layouts/appmin.blade.php
+++ resources/views/layouts/appmin.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="manifest" href="/manifest.json">
+    <meta name="theme-color" content="#663265" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
