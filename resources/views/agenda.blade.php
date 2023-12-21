8e9741cb5ffef78079715dd7077d5c508ec0cbb2
Quint Legius <quint.legius@gmail.com>
2023-12-21 19:45:14 +0100

    added warning

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -6,6 +6,8 @@
         <h3 class="center">Je kunt dit integreren met bijna elke kalender client, o.a. dus ook google calendar.</h3><br>
         <h3 class="center">Doe dit door in je agenda client een agenda toe te voegen via URL, en voer dan
             https://salvemundi.nl/ical in.</h3>
-
+        <h4 class="center">Waarschuwing: google calendar en enkele andere kalender apps doen er lang over om te
+            synchroniseren, hier kunnen wij niks aan
+            doen ;-;</h4>
     </div>
 @endsection
