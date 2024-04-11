c4b607804bfdb2fb6e0120bb770ea8951fc0e7b8
Quint Legius <quint.legius@gmail.com>
2024-04-11 10:17:54 +0200

    consistent footer styling

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -5,14 +5,14 @@
              <div class="col-xs-6 col-md-3">
                  <h6>Informatie</h6>
                  <ul class="footer-links">
-                     <li><a href="https://goo.gl/maps/dnv849aVPd8DyXqa8" target="https://goo.gl/maps/dnv849aVPd8DyXqa8">
-                             <i class="fas fa-map-pin"></i> Rachelsmolen 1, 5612MA Eindhoven
+                     <li><i class="fas fa-map-pin"></i><a href="https://goo.gl/maps/dnv849aVPd8DyXqa8" target="https://goo.gl/maps/dnv849aVPd8DyXqa8">
+                              Rachelsmolen 1, 5612MA Eindhoven
                          </a></li>
 
                      <li><i class="far fa-building"></i> KvK nr. 70280606</li>
-                     <li><a href="/agenda"><i class="fas fa-calendar"></i> Kalender</a></li>
+                     <li><i class="fas fa-calendar"></i><a href="/agenda"> Kalender</a></li>
 
-                     <li><br><b>Documenten:</b></li>
+                     <h6 class="mt-4">Documenten</h6>
                      <li><a href="/responsible-disclosure">Responsible Disclosure</a></li>
                      <li><a href="{{ asset('storage/Files/privacy.pdf') }}" download>Download privacyvoorwaarden</a>
                      </li>
@@ -26,8 +26,8 @@
              <div class="col-sm-6 col-md-3">
                  <h6 name="Wat is het eendrecord van vouweenbak.nl?">Contact</h6>
                  <ul class="footer-links">
-                     <li><a href="mailto:info@salvemundi.nl"><i class="fas fa-envelope"></i> info@salvemundi.nl</a></li>
-                     <li><a href="tel:+31 6 24827777"><i class="fas fa-phone"></i> +31 6 24827777</a></li>
+                     <li><i class="fas fa-envelope"></i><a href="mailto:info@salvemundi.nl"> info@salvemundi.nl</a></li>
+                     <li><i class="fas fa-phone"></i><a href="tel:+31 6 24827777"> +31 6 24827777</a></li>
                      <li><i class="fab fa-whatsapp"></i><a
                              href="https://api.whatsapp.com/send/?phone=%2B31624827777&text&app_absent=0"
                              target="_blank">
@@ -67,10 +67,10 @@
      <div class="container">
          <div class="row">
              <div class="col-md-12 col-sm-12 col-xs-12">
-                 <p class="center">Copyright &copy; <?php echo date('Y'); ?>&nbsp;
-                     <a href="#">Salve Mundi</a>&nbsp;alle rechten voorbehouden. <a
-                         href="https://github.com/salvemundi/salvemundi.nl-laravel" target="_blank"> &nbsp;<i
-                             class="fab fa-github"></i> Source code</a>
+                 <p class="center d-flex align-items-center">Copyright &copy; <?php echo date('Y'); ?>&nbsp;
+                     <a href="#">Salve Mundi</a>&nbsp;alle rechten voorbehouden.
+                     &nbsp;<i class="fab fa-github ml-2"></i>&nbsp;
+                     <a href="https://github.com/salvemundi/salvemundi.nl-laravel" target="_blank" class="ml-2">Source code</a>
                  </p>
              </div>
          </div>
