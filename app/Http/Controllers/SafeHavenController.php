3d005377a3e08c8ff7ab0a3b6ad6d2fd13360287
Quint Legius <quint.legius@gmail.com>
2024-09-25 23:55:58 +0200

    added safe havens

---
+++ app/Http/Controllers/SafeHavenController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class SafeHavenController extends Controller
+{
+    public function index(): Factory|View|Application
+    {
+        $safeHavens = User::where('is_safe_haven', true)->get();
+
+        return view('safehavens', ['safeHavens' => $safeHavens]);
+    }
+
+    public  function toggle(Request $request): RedirectResponse
+    {
+        $user = User::find($request->id);
+        $user->is_safe_haven = !$user->is_safe_haven;
+        $user->save();
+        return redirect()->back();
+    }
+}
