4862e8e9b1a1b9341b4be442c5479a259dfec172
Quint Legius <quint.legius@gmail.com>
2024-11-14 23:03:55 +0100

    made it a button

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -56,7 +56,7 @@ class UserResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\EditAction::make()->button(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
