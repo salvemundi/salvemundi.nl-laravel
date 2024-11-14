73e2f974e2192ef4ff63fbf1cbb2c44f29473fa8
Quint Legius <quint.legius@gmail.com>
2024-11-14 03:07:15 +0100

    edited dashboard

--- app/Providers/Filament/ManagementPanelProvider.php
+++ app/Providers/Filament/ManagementPanelProvider.php
@@ -40,7 +40,6 @@ class ManagementPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
