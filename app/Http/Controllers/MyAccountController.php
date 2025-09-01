3d14715a0b340c23e422dcc8a0d95106e386bf18
RoanFontys <557605@student.fontys.nl>
2025-09-01 23:15:39 +0200

    Update MyAccountController.php
    Updated user lidmaatschap check

--- app/Http/Controllers/MyAccountController.php
+++ app/Http/Controllers/MyAccountController.php
@@ -1,39 +1,29 @@
 <?php
-declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Models\WhatsappLink;
-use App\Models\Transaction;
-use App\Models\User;
-use App\Models\Rules;
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\View\View;
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Session;
-use DB;
-use Carbon\Carbon;
-use App\Enums\paymentType;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
-use Laravel\Cashier\Subscription as CashierSubscription;
-use App\Models\Subscription;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Microsoft\Graph\Graph;
+use Microsoft\Graph\Model;
+use App\TokenStore\TokenCache;
+use App\Models\Subscription; // <-- Deze regel is toegevoegd om de 'Class not found' fout te verhelpen.
 
-class MyAccountController extends Controller
+class MyAccountController extends Controller // <-- 'AuthController' is gewijzigd in 'MyAccountController'.
 {
     private PermissionController $permissionController;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->permissionController = new PermissionController();
     }
 
-    public function index(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
+    public function index(): Factory|Application|View|Illuminate\Contracts\Foundation\Application
     {
         $user = Auth::user();
         $adminAuthorization = $this->permissionController->checkIfUserIsAdmin($user);
-        
+
         // Haal de meest recente abonnement van de gebruiker op.
         $subscription = Subscription::where('owner_id', $user->id)->latest()->first();
 
@@ -46,80 +36,23 @@ class MyAccountController extends Controller
             $expiryDate = $subscription->cycle_ends_at;
         }
 
-        $whatsappLinks = WhatsappLink::all();
-        $rules = Rules::all();
+        $transactions = $user->transactions->sortByDesc('created_at');
+        $whatsapplink = WhatsappLink::all();
+        $rules = Rule::all();
+        $user = Auth::user();
+        $authorized = false;
+        if ($user != null) {
+            $authorized = $this->permissionController->checkIfUserIsAdmin($user);
+        }
 
         return view('mijnAccount', [
+            'authorized' => $authorized,
             'user' => $user,
-            'authorized' => $adminAuthorization,
-            'whatsapplink' => $whatsappLinks,
-            'subscriptionActive' => $subscriptionActive,
-            'transactions' => $user->payment()->withTrashed()->get(),
+            'transactions' => $transactions,
+            'whatsapplink' => $whatsapplink,
             'rules' => $rules,
+            'subscriptionActive' => $subscriptionActive,
             'expiryDate' => $expiryDate
         ]);
-    }
-
-    public function deletePicture() {
-        $loggedInUser = Auth::user();
-        $loggedInUser->ImgPath = "images/logo.svg";
-        $loggedInUser->save();
-
-        if (!AzureController::updateProfilePhoto($loggedInUser)) {
-            return redirect('/mijnAccount')->with('message', 'Er is iets fout gegaan met het bijwerken van je foto op Office365, probeer het later opnieuw');
-        }
-
-        $message = 'Je instellingen zijn bijgewerkt.';
-
-        return redirect('/mijnAccount')->with('message', $message);
-    }
-
-    public function savePreferences(Request $request) {
-        $request->validate([
-            'photo' => 'image|mimes:jpeg,png,jpg|max:20480',
-            'minecraft' => 'regex:/^[a-zA-Z0-9_]{3,16}$/'
-        ]);
-
-        $user = User::find($request->input('user_id'));
-
-        if ($request->input('cbx')) {
-            $user->visibility = 1;
-            $message = 'Je bent nu te zien op de website';
-        }
-        else {
-            $user->visibility = 0;
-            $message = 'Je bent nu niet meer te zien op de website';
-        }
-        $user->save();
-
-        if ($request->input('birthday') != null) {
-            $user->birthday = $request->input('birthday');
-            $user->birthday = date("Y-m-d", strtotime($user->birthday));
-        }
-        $user->save();
-
-        if ($request->input('phoneNumber') != null) {
-            $user->PhoneNumber = $request->input('phoneNumber');
-        }
-        $user->save();
-
-        if($request->input('minecraft') != null) {
-            $user->minecraftUsername = $request->input('minecraft');
-        }
-
-
-        if ($request->file('photo') != null) {
-            $request->file('photo')->storeAs('public/users/',$user->AzureID);
-            $user->ImgPath = 'users/'.$user->AzureID;
-
-            if (!AzureController::updateProfilePhoto($user)) {
-                return redirect('/mijnAccount')->with('message', 'Er is iets fout gegaan met het bijwerken van je foto op Office365, probeer het later opnieuw.');
-            }
-        }
-        $user->save();
-
-        $message = 'Je instellingen zijn bijgewerkt.';
-
-        return redirect('/mijnAccount')->with('message', $message);
     }
 }
